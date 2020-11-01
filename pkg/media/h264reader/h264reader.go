72b68f15a845f74ada9c8aef148900ecfbbe3844
Artur Shellunts <shellunts.artur@gmail.com>
2020-11-01 16:29:22 +0100

    Export NalUnitType and constants

--- pkg/media/h264reader/h264reader.go
+++ pkg/media/h264reader/h264reader.go
@@ -34,7 +34,7 @@ type NAL struct {
 	// NAL header
 	ForbiddenZeroBit bool
 	RefIdc           uint8
-	UnitType         nalUnitType
+	UnitType         NalUnitType
 
 	Data []byte // header byte + rbsp
 }
@@ -68,7 +68,7 @@ func (reader *H264Reader) ReadFrames() []NAL {
 		i = i + 1
 		nal := newNal()
 		nal.parseHeader(nalData[0])
-		if nal.UnitType == sei {
+		if nal.UnitType == SEI {
 			continue
 		}
 		nal.Data = nalData
@@ -166,91 +166,91 @@ func (h *findNalState) NalScan(data []byte) [][]byte {
 	return nals
 }
 
-type nalUnitType uint8
+type NalUnitType uint8
 
 const ( //   Table 7-1 NAL unit type codes
-	unspecified              nalUnitType = 0  // unspecified
-	codedSliceNonIdr         nalUnitType = 1  // Coded slice of a non-IDR picture
-	codedSliceDataPartitionA nalUnitType = 2  // Coded slice data partition A
-	codedSliceDataPartitionB nalUnitType = 3  // Coded slice data partition B
-	codedSliceDataPartitionC nalUnitType = 4  // Coded slice data partition C
-	codedSliceIdr            nalUnitType = 5  // Coded slice of an IDR picture
-	sei                      nalUnitType = 6  // Supplemental enhancement information (sei)
-	sps                      nalUnitType = 7  // Sequence parameter set
-	pps                      nalUnitType = 8  // Picture parameter set
-	aud                      nalUnitType = 9  // Access unit delimiter
-	endOfSequence            nalUnitType = 10 // End of sequence
-	endOfStream              nalUnitType = 11 // End of stream
-	filler                   nalUnitType = 12 // filler data
-	spsExt                   nalUnitType = 13 // Sequence parameter set extension
+	Unspecified              NalUnitType = 0  // Unspecified
+	CodedSliceNonIdr         NalUnitType = 1  // Coded slice of a non-IDR picture
+	CodedSliceDataPartitionA NalUnitType = 2  // Coded slice data partition A
+	CodedSliceDataPartitionB NalUnitType = 3  // Coded slice data partition B
+	CodedSliceDataPartitionC NalUnitType = 4  // Coded slice data partition C
+	CodedSliceIdr            NalUnitType = 5  // Coded slice of an IDR picture
+	SEI                      NalUnitType = 6  // Supplemental enhancement information (SEI)
+	SPS                      NalUnitType = 7  // Sequence parameter set
+	PPS                      NalUnitType = 8  // Picture parameter set
+	AUD                      NalUnitType = 9  // Access unit delimiter
+	EndOfSequence            NalUnitType = 10 // End of sequence
+	EndOfStream              NalUnitType = 11 // End of stream
+	Filler                   NalUnitType = 12 // Filler data
+	SpsExt                   NalUnitType = 13 // Sequence parameter set extension
 	// 14..18           // Reserved
-	nalUnitTypeCodedSliceAux nalUnitType = 19 // Coded slice of an auxiliary coded picture without partitioning
+	NalUnitTypeCodedSliceAux NalUnitType = 19 // Coded slice of an auxiliary coded picture without partitioning
 	// 20..23           // Reserved
-	// 24..31           // unspecified
+	// 24..31           // Unspecified
 )
 
-func nalUnitTypeStr(v nalUnitType) string {
+func NalUnitTypeStr(v NalUnitType) string {
 	str := "Unknown"
 	switch v {
 	case 0:
 		{
-			str = "unspecified"
+			str = "Unspecified"
 		}
 	case 1:
 		{
-			str = "codedSliceNonIdr"
+			str = "CodedSliceNonIdr"
 		}
 	case 2:
 		{
-			str = "codedSliceDataPartitionA"
+			str = "CodedSliceDataPartitionA"
 		}
 	case 3:
 		{
-			str = "codedSliceDataPartitionB"
+			str = "CodedSliceDataPartitionB"
 		}
 	case 4:
 		{
-			str = "codedSliceDataPartitionC"
+			str = "CodedSliceDataPartitionC"
 		}
 	case 5:
 		{
-			str = "codedSliceIdr"
+			str = "CodedSliceIdr"
 		}
 	case 6:
 		{
-			str = "sei"
+			str = "SEI"
 		}
 	case 7:
 		{
-			str = "sps"
+			str = "SPS"
 		}
 	case 8:
 		{
-			str = "pps"
+			str = "PPS"
 		}
 	case 9:
 		{
-			str = "aud"
+			str = "AUD"
 		}
 	case 10:
 		{
-			str = "endOfSequence"
+			str = "EndOfSequence"
 		}
 	case 11:
 		{
-			str = "endOfStream"
+			str = "EndOfStream"
 		}
 	case 12:
 		{
-			str = "filler"
+			str = "Filler"
 		}
 	case 13:
 		{
-			str = "spsExt"
+			str = "SpsExt"
 		}
 	case 19:
 		{
-			str = "nalUnitTypeCodedSliceAux"
+			str = "NalUnitTypeCodedSliceAux"
 		}
 	default:
 		{
@@ -262,11 +262,11 @@ func nalUnitTypeStr(v nalUnitType) string {
 }
 
 func newNal() NAL {
-	return NAL{PictureOrderCount: 0, ForbiddenZeroBit: false, RefIdc: 0, UnitType: unspecified, Data: make([]byte, 0)}
+	return NAL{PictureOrderCount: 0, ForbiddenZeroBit: false, RefIdc: 0, UnitType: Unspecified, Data: make([]byte, 0)}
 }
 
 func (h *NAL) parseHeader(firstByte byte) {
 	h.ForbiddenZeroBit = (((firstByte & 0x80) >> 7) == 1) // 0x80 = 0b10000000
 	h.RefIdc = (firstByte & 0x60) >> 5                    // 0x60 = 0b01100000
-	h.UnitType = nalUnitType((firstByte & 0x1F) >> 0)     // 0x1F = 0b00011111
+	h.UnitType = NalUnitType((firstByte & 0x1F) >> 0)     // 0x1F = 0b00011111
 }
